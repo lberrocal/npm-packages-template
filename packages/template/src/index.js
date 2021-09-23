d55f2cae1ab27a5c5333e7e800689e67d20fcba8
lberrocal <luisberrocal@outlook.com>
2021-09-24 01:06:05 +0200

    fix(template): Testing fix
    Testing fix version strategy with monodeploy

--- packages/template/src/index.js
+++ packages/template/src/index.js
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class Template extends Component {
   render() {
-    return <div>Basic Component v0.0.0</div>;
+    return <div>Basic Component v0.0.1</div>;
   }
 }
