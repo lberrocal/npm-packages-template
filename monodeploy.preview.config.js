0a59a6fc078cd84ea7fc9c51d96e4bc52c623d9c
lberrocal <luisberrocal@outlook.com>
2021-09-24 01:04:38 +0200

    build(Monodeploy): Configuring monodeploy scripts
    Adding preview build scripts

---
+++ monodeploy.preview.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  persistVersions: false,
+  dryRun: true,
+  changesetFilename: '-',
+  conventionalChangelogConfig: '@tophat/conventional-changelog-config',
+  changesetIgnorePatterns: ['**/*.test.js'],
+  changelogFilename: "<packageDir>/CHANGELOG.md"
+}
