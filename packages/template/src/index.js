f95b4ab5ff9b0576ca1e0e539fde0dab7be2d50d
lberrocal <luisberrocal@outlook.com>
2021-09-27 10:06:26 +0200

    feat: testing minor release
    Testing minor release

--- packages/template/src/index.js
+++ packages/template/src/index.js
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class Template extends Component {
   render() {
-    return <div>Basic Component v0.1.1</div>;
+    return <div>Basic Component v0.2.0</div>;
   }
 }
